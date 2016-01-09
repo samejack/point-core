e668774ff5a534f9765e3d3050c90322cf081a45
SJ Chou <sj@toright.com>
2016-01-10 00:53:17 +0800

    fix build

--- Autoloader.php
+++ Autoloader.php
@@ -5,3 +5,6 @@ include_once dirname(__FILE__) . '/src/Context.php';
 include_once dirname(__FILE__) . '/src/Bootstrap.php';
 
 include_once dirname(__FILE__) . '/src/Framework.php';
+
+if (file_exists(dirname(__FILE__) . '/vendor/autoload.php'))
+    include_once dirname(__FILE__) . '/vendor/autoload.php';
