55a572ac0bb7d712aa79731b981bb2214e0ca0d4
SJ Chou <sj@toright.com>
2016-01-10 00:48:56 +0800

    fix autoloader

--- Autoloader.php
+++ Autoloader.php
@@ -5,5 +5,3 @@ include_once dirname(__FILE__) . '/src/Context.php';
 include_once dirname(__FILE__) . '/src/Bootstrap.php';
 
 include_once dirname(__FILE__) . '/src/Framework.php';
-
-include_once dirname(__FILE__) . '/vendor/autoload.php';
