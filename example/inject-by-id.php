0c7bae08094100591a40de06d2f6f078713c8865
SJ Chou <sj@toright.com>
2017-01-20 19:57:08 +0800

    @Qualifier inject example

--- example/inject-by-id.php
+++ example/inject-by-id.php
@@ -7,31 +7,49 @@ use point\core\Bean;
 
 class Foo
 {
-  /**
-   * @Autowired
-   * @var Bar
-   */
-  private $_bar;
+    /**
+     * @Qualifier("bar.2")
+     * @var Bar
+     */
+    private $_bar;
 
-  public function getBar()
-  {
-      return $this->_bar;
-  }
+    public function getBar()
+    {
+        return $this->_bar;
+    }
 }
 
 class Bar
 {
+    private $_name;
+
+    public function __construct($name)
+    {
+        $this->_name = $name;
+    }
+
+    public function toString()
+    {
+        return $this->_name;
+    }
 }
 
 $context = new Context();
 
 $context->addConfiguration(array(
-  array(
-    Bean::CLASS_NAME => 'Foo'
-  ),
-  array(
-    Bean::CLASS_NAME => 'Bar'
-  )
+    array(
+        Bean::CLASS_NAME => 'Foo'
+    ),
+    array(
+        Bean::CLASS_NAME => 'Bar',
+        Bean::ID => 'bar.1',
+        Bean::CONSTRUCTOR_ARG => ['i am first.']
+    ),
+    array(
+        Bean::CLASS_NAME => 'Bar',
+        Bean::ID => 'bar.2',
+        Bean::CONSTRUCTOR_ARG => ['i am second.']
+    )
 ));
 
 $foo = $context->getBeanByClassName('Foo');
